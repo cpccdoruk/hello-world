a8b9e122ee776c49b465bf27f45142f74c37659a
cpccdoruk <bilkili0@email.cpcc.edu>
2026-02-16 19:32:56 -0500

    added 1 more line
    a new line added

--- helloworldAdJava.java
+++ helloworldAdJava.java
@@ -1,5 +1,6 @@
 public class helloworldAdJava {
     public static void main(String[] args) {
-   System.out.print("hello world Advanced Java");
+   System.out.println("hello world Advanced Java");
+   System.out.print("Program modified, but able to revert to original version using GitHub Desktop");
    }
    }
